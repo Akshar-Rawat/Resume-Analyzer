c07ca0f8e026aab4154623fee007513c4ee84125
Akshar-Rawat <aksharrawat7@gmail.com>
2025-10-25 00:58:34 +0530

    Resume feedback components

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import ScoreGauge from './ScoreGauge'
+import ScoreBadge from './ScoreBadge';
+const Category=({title,score})=>{
+  const textColor=score>70?'text-green-600':
+  score>49?'text-yellow-600':'text-red-600';
+  return(
+<div className='resume-summary'>
+<div className='category'> 
+ <div className='flex flex-row gap-2 items-center justify-center'>
+  <p>{title}</p>
+  <ScoreBadge score={score}/>
+ </div>
+ <p className='text-2xl'>
+<span className='{textColor}'>
 
-const Summary = () => {
+</span>
+ </p>
+</div>
+
+</div>)
+}
+
+const Summary = ({feedback}) => {
   return (
-    <div>
-      
+    <div className='bg-white rounded-2xl shadow-md w-full'>
+      <div className='flex flex-row items-center p-4 gap-8'>
+     <ScoreGauge score={feedback.overallScore}/>   
+  <div className='flex flex-col gap-2'>
+    <h2 className='text-2xl font-bold '>
+Your Resume Score
+    </h2>
+    <p className='text-sm text-gray-500'>
+This score is calculated based on the variables listed below
+    </p>
+  </div>
+      </div>
+
+<Category>
+
+</Category>
     </div>
   )
 }
